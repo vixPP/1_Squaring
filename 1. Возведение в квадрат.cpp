01f196b08ded9a7f052d8ff409bf786d19a8daae
vix <churink9@gmail.com>
2024-06-23 19:14:37 +0300

    first commit

---
+++ 1. Возведение в квадрат.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+
+template<typename T>
+T square(T x)
+{
+    std::cout << "[IN]: " << x << std::endl;
+    return x * x;
+}
+
+
+template<typename T>
+std::vector<T> square(const std::vector<T>& v)
+{
+
+    std::vector<T> result;
+
+    std::cout << "[IN]: ";
+    for (int i = 0; i < v.size(); ++i) 
+    {
+        std::cout << v[i];
+        if (i < v.size() - 1) 
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl; 
+
+    for (const auto& elem : v) 
+    {
+        result.push_back(elem * elem);
+    }
+    return result;
+}
+
+
+
+int main()
+{
+    std::cout << "[OUT]: " << square(4) << std::endl;
+    
+    std::vector<int> input = { -1, 4, 8 };
+
+    std::vector<int> result = square(input);
+    std::cout << "[OUT]: ";
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << result[i];
+        if (i < result.size() - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+}
+
